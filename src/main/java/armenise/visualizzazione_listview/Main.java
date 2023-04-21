5fac401d5ff5a3baa2bb5a402fd5550ce8d4d53b
Marco Armenise <armenise.marco@panettipitagora.edu.it>
2023-04-22 01:01:42 +0200

    Modificati file

--- src/main/java/armenise/visualizzazione_listview/Main.java
+++ src/main/java/armenise/visualizzazione_listview/Main.java
@@ -10,14 +10,12 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-
         //Carichiamo la HomePage
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("HomePage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("HomePage");
         stage.setScene(scene);
         stage.show();
-
     }
 
     public static void main(String[] args) {
